3dd28ededea6d29ff3d2204b42d68febc7553f47
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-02-08 19:01:49 +0200

    💠 EventCard: add event date

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -20,6 +20,15 @@ export default function EventCard({ event }: EventCardProps) {
         <p className="italic text-white/75">By {event.organizerName}</p>
         <p className="text-sm text-white/50 mt-4">{event.location}</p>
       </div>
+
+      <section className="absolute flex flex-col justify-center items-center left-[12px] top-[12px] h-[45px] w-[45px] bg-black/30 rounded-md">
+        <p className="text-xl font-bold -mb-[5px]">
+          {new Date(event.date).toLocaleDateString("en-US", { day: "2-digit" })}
+        </p>
+        <p className="text-xs uppercase text-accent">
+          {new Date(event.date).toLocaleDateString("en-US", { month: "short" })}
+        </p>
+      </section>
     </section>
   );
 }
