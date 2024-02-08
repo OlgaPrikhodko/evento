72c3c04928c35420790a24704efba47845c4e662
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-02-08 11:20:28 +0200

    :star: EventsList: fetch and display events

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,28 @@
-export default function EventsPage() {
-  return <div>Events Page</div>;
+import EventsList from "@/components/events-list";
+import H1 from "@/components/h1";
+import { EventoEvent } from "@/lib/types";
+
+type EventsPageProps = {
+  params: { city: string };
+};
+
+export default async function EventsPage({ params }: EventsPageProps) {
+  const response = await fetch(
+    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+  );
+  const events: EventoEvent[] = await response.json();
+
+  const city = params.city;
+
+  return (
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+      <H1>
+        {city === "all"
+          ? "All Events"
+          : `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+      </H1>
+
+      <EventsList events={events} />
+    </main>
+  );
 }
