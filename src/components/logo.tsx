3c2502418516dc9c4bb4b8290b06c994bf1dcb3e
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-02-08 09:31:02 +0200

    :star: wrap logo in link to home page

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Image src="/eventoLogo.png" alt="EVENTO Logo" width={53} height={12} />
+    <Link href="/">
+      <Image src="/eventoLogo.png" alt="EVENTO Logo" width={53} height={12} />
+    </Link>
   );
 }
