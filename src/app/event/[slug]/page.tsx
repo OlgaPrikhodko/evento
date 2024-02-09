8e9733749108183efbb2dcb3b70f2ee25342329f
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-02-09 18:14:19 +0200

    :star: add loading page for event page

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,12 +1,16 @@
 import H1 from "@/components/h1";
 import { EventoEvent } from "@/lib/types";
+import { sleep } from "@/lib/utils";
 import Image from "next/image";
 
 type EventPageProps = {
   params: { slug: string };
 };
+
 export default async function EventPage({ params }: EventPageProps) {
   const { slug } = params;
+
+  await sleep(2000);
 
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
