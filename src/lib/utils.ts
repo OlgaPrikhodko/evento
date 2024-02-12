43cba1913a0b4cbceeb493340f1185baf1757c44
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-02-12 14:24:00 +0200

    :star: server-only utilities
    Prevent accidental usage of utility functions on the client.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,5 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import prisma from "./db";
-import { notFound } from "next/navigation";
-import { unstable_cache } from "next/cache";
 
 // combine twMerge and clsx functions for conditions classes
 export function cn(...inputs: ClassValue[]) {
@@ -16,37 +13,3 @@ export async function sleep(ms: number) {
 export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
-
-export const getEvent = unstable_cache(async (slug: string) => {
-  const event = await prisma.eventoEvent.findUnique({
-    where: {
-      slug,
-    },
-  });
-
-  if (!event) {
-    return notFound();
-  }
-
-  return event;
-});
-
-export const getEvents = unstable_cache(async (city: string, page = 1) => {
-  const events = await prisma.eventoEvent.findMany({
-    where: { city: city === "all" ? undefined : capitalize(city) },
-    orderBy: { date: "asc" },
-    take: 6,
-    skip: (page - 1) * 6,
-  });
-
-  let totalCount;
-  if (city === "all") {
-    totalCount = await prisma.eventoEvent.count();
-  } else {
-    totalCount = await prisma.eventoEvent.count({
-      where: { city: city === "all" ? undefined : capitalize(city) },
-    });
-  }
-
-  return { events, totalCount };
-});
