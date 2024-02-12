08e79b6698558f62a455e19e1a7677f84aa5e8a2
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-02-12 10:20:03 +0200

    :star: pris,a orm caching with unstable_cache

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import prisma from "./db";
 import { notFound } from "next/navigation";
+import { unstable_cache } from "next/cache";
 
 // combine twMerge and clsx functions for conditions classes
 export function cn(...inputs: ClassValue[]) {
@@ -16,7 +17,7 @@ export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-export async function getEvent(slug: string) {
+export const getEvent = unstable_cache(async (slug: string) => {
   const event = await prisma.eventoEvent.findUnique({
     where: {
       slug,
@@ -28,9 +29,9 @@ export async function getEvent(slug: string) {
   }
 
   return event;
-}
+});
 
-export async function getEvents(city: string, page = 1) {
+export const getEvents = unstable_cache(async (city: string, page = 1) => {
   const events = await prisma.eventoEvent.findMany({
     where: { city: city === "all" ? undefined : capitalize(city) },
     orderBy: { date: "asc" },
@@ -48,4 +49,4 @@ export async function getEvents(city: string, page = 1) {
   }
 
   return { events, totalCount };
-}
+});
